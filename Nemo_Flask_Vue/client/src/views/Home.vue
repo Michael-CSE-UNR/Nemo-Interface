371156b33196af66893311a606b34e0b1e65df9d
Michael <96790637+mmoosmuller@users.noreply.github.com>
2022-12-16 13:30:24 -0800

    Edited Navigation bar to display NEMO as the home and How to Use as the about page. Added a DropBox.vue file to handle the drop box components of the webpage.

--- Nemo_Flask_Vue/client/src/views/Home.vue
+++ Nemo_Flask_Vue/client/src/views/Home.vue
@@ -8,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+// import DropBox from '@/components/DropBox.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+    // DropBox
   }
 }
 </script>
---
+++ Nemo_Flask_Vue/client/src/views/HowToUse.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="howtouse">
+    <h1>This is a how to use page</h1>
+  </div>
+</template>
