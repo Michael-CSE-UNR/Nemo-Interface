db3242c62dc8a988eba3199558dee3ea13406996
Michael-CSE-UNR <96790637+Michael-CSE-UNR@users.noreply.github.com>
2022-12-29 14:34:17 -0800

    Add files via upload

---
+++ NEMO_UI/client/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import {createRouter, createWebHistory} from 'vue-router'
+import App from './App.vue'
+import Home from './views/Home.vue'
+import HowToUse from './views/HowToUse.vue'
+import Contribute from './views/Contribute.vue'
+import "bootstrap/dist/css/bootstrap.css";
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', name: 'Home', component: Home},
+        {path: '/howtouse', name: 'HowToUse', component: HowToUse},
+        {path: '/contribute', name: 'Contribute', component: Contribute}
+    ]
+})
+
+createApp(App)
+    .use(router)
+    .mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
---
+++ NEMO_UI/client/src/views/Contribute.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Contribute">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Contribute"
+}
+</script>
+
+<style scoped>
+
+</style>
