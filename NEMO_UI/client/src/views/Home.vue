b3cdf93022cae7e515eb2e412c2e695a80b47d11
Michael <96790637+mmoosmuller@users.noreply.github.com>
2022-12-30 12:32:57 -0800

    Version 2: Corrected MenuBar issue with transparency. Added simple file upload drop box.

--- NEMO_UI/client/src/views/Home.vue
+++ NEMO_UI/client/src/views/Home.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="Home">
     <About />
+    <FileUpload />
   </div>
 </template>
 
 <script>
 import About from "@/components/About.vue";
+import FileUpload from "@/components/FileUpload.vue";
 
 export default {
   name: "Home",
-  components: {About}
+  components: {About, FileUpload}
 }
 </script>
